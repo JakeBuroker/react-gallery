81e2727cc5640a5c6fa05eab4fe6cf83e3c86c19
Jake Buroker <shamrecks@gmail.com>
2024-01-12 14:11:58 -0600

    get and put routes operational

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const pool = require("../modules/pool");
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
-  // code here
+  let galleryItemId = req.params.id;
+  let itemLikes = req.body.likes;
+  console.log("Shopping List ID:", req.body);
+  console.log(itemLikes)
+  const queryText = `
+    UPDATE "gallery" SET "likes" = "likes" + 1 WHERE "id" = $1;
+    `;
+  // declare queryParams for ID
+  const queryParams = [galleryItemId];
+  // send UPDATE to DB
+  pool
+    .query(queryText, queryParams)
+    // then send ok status
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log("error:", error);
+    });
 });
 
 // GET /gallery
-router.get('/', (req, res) => {
-  // code here
+router.get("/", (req, res) => {
+  // When you fetch all things in these GET routes, strongly encourage ORDER BY
+  // so that things always come back in a consistent order
+  const sqlText = `SELECT * FROM gallery`;
+  pool
+    .query(sqlText)
+    .then((result) => {
+      console.log(`Got stuff back from the database`, result);
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(`Error making database query ${sqlText}`, error);
+      res.sendStatus(500); // Good server always responds
+    });
 });
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,8 @@
 
+import React from "react";
+import axios from "axios";
+
+
 function App() {
     return (
       <div>
@@ -8,6 +12,8 @@ function App() {
 
         <p>The gallery goes here!</p>
         <img src="images/goat_small.jpg"/>
+        <img src="images/Hello.jpg"/>
+        <img src="images/2023-lucid-air-1.jpg"/>
       </div>
     );
 }
